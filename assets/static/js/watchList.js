8c09167df3758598c23b2371521b304411059059
Thinkedou <r.enguehard@gmail.com>
2022-10-10 14:36:59 +0200

    change directories

---
+++ assets/static/js/watchList.js
@@ -0,0 +1,23 @@
+export const userWatchList = [
+    {
+        "series":"Better Call Saul",
+        "season":4,
+        "episode":8,
+        "timeCursor":"00:42:15"
+
+    },
+    {
+        "series":"The Leftovers",
+        "season":2,
+        "episode":2,
+        "timeCursor":"00:12:15"
+
+    },
+    {
+        "series":"The OA",
+        "season":1,
+        "episode":3,
+        "timeCursor":"00:00:00"
+
+    }
+]
